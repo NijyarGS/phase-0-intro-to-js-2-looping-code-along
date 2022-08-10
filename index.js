25560c11890785bf33ce3528b08ba537c08d98c9
Nijyar Ghazi <nijyar360@gmail.com>
2022-08-10 03:18:42 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+function writeCards(array,eventName)
+{
+    let newArray = [];
+    for (let i=0;i<array.length;i++)
+    {
+        newArray[i]=`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`
+    
+    }
+    
+    return newArray;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+function countDown(e) {
+    if (e>0)
+    {
+        while (e>=0)
+        {
+            console.log(e)
+            e=e-1;
+        }
+    }
+}
